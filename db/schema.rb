aaa516ca10210f2fc3a09b1896d5393c6663abf2
Chris Scott <cyberstrike@gmail.com>
2015-05-02 13:42:23 -0400

    merged up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502160342) do
+ActiveRecord::Schema.define(version: 20150502165325) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(version: 20150502160342) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-
-ActiveRecord::Schema.define(version: 20150502165325) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
