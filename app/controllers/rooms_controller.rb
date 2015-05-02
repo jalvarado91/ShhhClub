b1c4c472253f21d3378fa7d2347df9313b817060
Julissa Cotillo <julissacotillo@gmail.com>
2015-05-02 16:40:24 -0400

    another column

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,25 +1,46 @@
 class RoomsController < ApplicationController
+  
   def index
   	@rooms = Room.all
+  	render :json => @rooms
   end
    
   def show
+  	@room = Room.find(params[:id])
+  	render :json => @room
   end
 
   def new
-  	@room = current_user.Room.new
+  	@room = Room.new
   end
 
   def create
+  	@room = Room.new room_params
+  	@room.dj = current_user
+  	if @room.save!
+  		render :json => @room 
+  	end
   end
 	 
   def edit
+  	@room = Room.find(params[:id])
   end
 
   def update
+  	@room = Room.find(params[:id])
+  	@room.update room_params
+  	if @room.save
+  	render :json => @room 
+ 	end
   end
 
   def destroy
+  	@room = Room.find(params[:id])
+  	@room.destroy
   end
-
+ 
+ private
+  def room_params
+    params.require(:room).permit(:title, :description, :private, :youtube_url)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502193751) do
+ActiveRecord::Schema.define(version: 20150502202932) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150502193751) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "dj_id"
+    t.string   "location"
   end
 
   create_table "users", force: :cascade do |t|
