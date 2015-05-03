d1e83e3c93802ffe8e3ff8585c4d4de7de181ea7
Chris Scott <cyberstrike@gmail.com>
2015-05-03 08:58:04 -0400

    facebook fix 20

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,8 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email if user.email.empty?
-      user.password = Devise.friendly_token[0,20] if user.email.empty?
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
       # user.name = auth.info.name   # assuming the user model has a name
       # user.image = auth.info.image # assuming the user model has an image
 
