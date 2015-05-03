e824a5605c3c051eb89a37aa85860cd40a6a1757
Chris Scott <cyberstrike@gmail.com>
2015-05-02 22:22:06 -0400

    try two

--- app/models/soundcloudapi.rb
+++ app/models/soundcloudapi.rb
@@ -1,7 +1,5 @@
 class SoundCloudApi
-
-  require 'Soundcloud'
-
-  client = SoundCloud.new(:client_id => '25cc17a15dbfaef9d51ce58a2bb45ae5')
+  
+  client = Soundcloud.new(:client_id => '25cc17a15dbfaef9d51ce58a2bb45ae5')
 
 end
